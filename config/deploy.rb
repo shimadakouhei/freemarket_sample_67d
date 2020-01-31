b02977a3ef64f79a29df9fb0943af91cf1ee0c39
shimadakouhei <shimapykkm@gmail.com>
2020-01-31 17:47:42 +0900

    記述ミス修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'freemarket'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:shimadakouhei/freemarket.git'
+set :repo_url,  'git@github.com:shimadakouhei/freemarket_sample_67d.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
