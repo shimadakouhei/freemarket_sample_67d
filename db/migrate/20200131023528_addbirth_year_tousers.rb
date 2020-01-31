7a97c876a0d48bb4d939c330e9c03409f70fb8b5
yukihiro3710 <59110433+yukihiro3710@users.noreply.github.com>
2020-01-31 14:41:50 +0900

    更新（マイぐれ

--- db/migrate/20200131023528_addbirth_year_tousers.rb
+++ db/migrate/20200131023528_addbirth_year_tousers.rb
@@ -1,5 +1,5 @@
 class AddbirthYearTousers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :birth_year, :integerm, null: false 
+    add_column :users, :birth_year, :integer, null: false 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_045220) do
+ActiveRecord::Schema.define(version: 2020_01_31_025701) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2020_01_30_045220) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname", null: false
+    t.string "family_name", null: false
+    t.string "hurigana_family_name", null: false
+    t.string "hurigana_first_name", null: false
+    t.integer "birth_year", null: false
+    t.integer "birth_month", null: false
+    t.integer "birth_date", null: false
+    t.string "first_name", null: false
+    t.string "password", null: false
+    t.string "password_confiramation", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
