2136bdbdcafd2af8157dd797f9a85fad919dc093
shimadakouhei <shimapykkm@gmail.com>
2020-02-01 16:00:26 +0900

    showアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
-  resources :top, only: [:index, :new, :create]
+  resources :top, only: [:index, :new, :create, :show]
   resources :users, only: [:index, :show]
 end
