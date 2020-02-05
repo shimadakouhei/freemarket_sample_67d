eda5383fedf876943842f079092ff48723576aba
yukihiro3710 <59110433+yukihiro3710@users.noreply.github.com>
2020-02-05 21:25:40 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, if: :production?
+
+
+
+
+  private
 
   def production?
     Rails.env.production?
