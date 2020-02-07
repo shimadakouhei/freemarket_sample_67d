b61bf59ae313eac8111e704d31745eb483775934
annafukuzaki <aaannnnnnaaa60@gmail.com>
2020-02-07 16:58:55 +0900

    fixed

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,4 @@
 class Image < ApplicationRecord
-t.string :src
-t.references :product, foreign_key: true
 mount_uploader :src, ImageUploader
 belongs_to :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
 
 has_many :images
 accepts_nested_attributes_for :images
+accepts_nested_attributes_for :images, allow_destroy: true
 
   belongs_to :category
 end
