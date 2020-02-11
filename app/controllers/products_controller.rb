8c2557d6da48c6092581167038dfef935f397d12
matsukiza1104 <matsukiza1104@gmail.com>
2020-02-11 21:01:47 +0900

    productコントラーらー少し編集しました

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,21 @@
 class ProductsController < ApplicationController
-  class ProductsController < ApplicationController
-    def index
-      @products = Product.includes(:images).order('created_at DESC')
-    end
-  
-    def new
-    end
-  
-    def create
-    end
-  
-    def edit
-      @products = Product.find(params[:id])
-    end
-  
-    def update
-    end
-  
-    def destroy
-    end
+  def index
+    @products = Product.includes(:images).order('created_at DESC')
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+    @products = Product.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
