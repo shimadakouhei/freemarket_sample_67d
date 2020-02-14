6586fbbeb4aa31d03d93300ae331c02a259a4876
shimadakouhei <shimapykkm@gmail.com>
2020-02-14 18:20:10 +0900

    ファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -33,10 +33,11 @@ ActiveRecord::Schema.define(version: 2020_02_14_062911) do
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "src"
+    t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "src"
-    t.integer "product_id"
+    t.index ["product_id"], name: "index_images_on_product_id"
   end
 
   create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -83,4 +84,5 @@ ActiveRecord::Schema.define(version: 2020_02_14_062911) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "products"
 end
