835d250653b357e26f03b913d6aef5f444fa63a0
nishida-boop <59200992+nishida-boop@users.noreply.github.com>
2020-02-14 17:28:32 +0900

    kkkkkkk

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-    before_action :set_product, only: [:destroy :show]
+    before_action :set_product, only: [:destroy, :show]
   def index
     @products = Product.includes(:images).order('created_at DESC')
   end
