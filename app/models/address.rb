518ae163f2bbc861158c59e577ac80111dac9593
yukihiro3710 <59110433+yukihiro3710@users.noreply.github.com>
2020-02-17 13:12:37 +0900

    更新　秘密鍵

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,6 @@
 class Address < ApplicationRecord
-  validates :postal_code, presence: true, format:{ with: /\A\d{7}\z/, message: 'はで入力をお願いします'}
-  validates :administrative＿divisions, presence: true,format:{ with: /\A[ぁ-んー－]+\z/, message: 'は漢字で入力をお願いします'}
-  validates :city, presence: true
+  validates :postal_code, presence: true, length: { maximum: 8, too_long: 'は8文字以内で記入してください'}
+  validates :administrative＿divisions, presence: true
   validates :street_number, presence: true
   validates :tel,length: { maximum: 13, too_long: 'は8文字以内で記入してください'}
   
