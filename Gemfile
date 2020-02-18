f47ea7e6ef38f349ee01da8460aa8675fdf1765a
matsukiza1104 <matsukiza1104@gmail.com>
2020-02-18 12:47:25 +0900

    編集機能終了

--- Gemfile
+++ Gemfile
@@ -82,3 +82,4 @@ gem 'jquery-rails'
 gem 'pry-rails'
 gem 'active_hash'
 gem "gretel"
+gem 'dotenv-rails' 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -50,7 +50,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :conditions, :delivery_charge, :prefecture, :delivery_day, :text, :user_id, :category_id, :brand_id, :price, images_attributes:  [:src, :_destroy, :product_id]).merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :conditions, :delivery_charge, :prefecture, :delivery_day, :text, :category_id, :brand_id, :price, images_attributes:  [:src, :_destroy, :id]).merge(user_id: current_user.id)
   end
 
  
